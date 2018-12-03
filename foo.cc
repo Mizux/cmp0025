15e4a3208207ebd1d45dfccf8103417943d4e76d
Corentin Le Molgat <corentinl@google.com>
2018-12-03 11:17:07 +0100

    Test both methods

--- foo.cc
+++ foo.cc
@@ -3,18 +3,22 @@
 using namespace std;
 
 int main() {
+#if defined(USE_LLVM_SET)
+  cout << "Using Clang or AppleClang based compiler (use set) !" << endl;
+#endif
+#if defined(USE_LLVM_OR)
+  cout << "Using Clang or AppleClang based compiler (use or) !" << endl;
+#endif
+
 #if defined(USE_CLANG)
   cout << "Using Clang compiler !" << endl;
 #endif
 #if defined(USE_APPLE_CLANG)
   cout << "Using Apple Clang compiler !" << endl;
 #endif
-#if not defined(USE_CLANG) && not defined(USE_APPLE_CLANG)
-  cout << "using unknow compiler !" << endl;
-#endif
 
-#if defined(USE_LLVM)
-  cout << "Using an LLVM based compiler (Clang or AppleClang) !" << endl;
+#if !defined(USE_CLANG) && !defined(USE_APPLE_CLANG)
+  cout << "using unknow compiler !" << endl;
 #endif
   return 0;
 }
